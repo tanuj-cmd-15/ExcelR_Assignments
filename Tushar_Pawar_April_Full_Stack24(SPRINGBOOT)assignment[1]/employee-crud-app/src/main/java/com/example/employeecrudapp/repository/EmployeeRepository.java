6688144ee79f5d8e1b5d3ce36ed00c4c38898cd2
TUSHAR PAWAR <88785532+tanuj-cmd-15@users.noreply.github.com>
2024-09-22 11:00:36 +0530

    Springboot

--- Tushar_Pawar_April_Full_Stack24(SPRINGBOOT)assignment[1]/employee-crud-app/src/main/java/com/example/employeecrudapp/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.employeecrudapp.repository;
-
-import com.example.employeecrudapp.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
